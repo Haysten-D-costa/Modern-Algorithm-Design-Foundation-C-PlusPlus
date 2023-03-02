81bf425c10275dfa2760f619b603d80640472a21
Haysten Dcosta <haystendc@gmail.com>
2023-03-02 20:00:32 +0530

    Added New Experiment file, and edited the comments in experiment file 01

---
+++ Experiment02.cpp
@@ -0,0 +1,14 @@
+/* *****************************************************************************************************************
+Lab ID        : 2.0
+Program Title : Merge Sort using Divide and Conquer
+Author        : Haysten D'costa
+Roll No.      : 21co56
+Class         : Comp B[Batch P3]
+Language      : C++
+Due Date      : --
+--------------------------------------------------------------------------------------------------------------------
+Description   : Program to implement Merge Sort algorithm using Divide and Conquer...
+Algorithm     : --
+Prerequisites : C or C++
+Known Bugs    : NONE
+***************************************************************************************************************** */
