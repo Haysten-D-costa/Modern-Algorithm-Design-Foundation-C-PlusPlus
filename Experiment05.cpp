c732e5e73246c1566d667dc03a17ce7677a567dd
Haysten Dcosta <haystendc@gmail.com>
2023-04-21 21:36:16 +0530

    Created my own code for DIJKSTRAS Algorithm...

--- Experiment05.cpp
+++ Experiment05.cpp
@@ -26,28 +26,28 @@ void displayContent(int Cost[][10], int Distance[], bool S[], int n); // Functio
 int main()
 {
     bool S[10];
-    int n = 6, Distance[10];
-    int Cost[10][10] = { /* Matrix : Example '01' Given by teacher... VERIFIED*/
-        {I , 50, 45, 10, I , I },
-        {I , I , 10, 15, I , I },
-        {I , I , I , I , 30, I },
-        {20, I , I , I , 15, I },
-        {I , 20, 35, I , I , I },
-        {I , I , I , I , 3 , I }
-    };
+    int n = 8, Distance[10];
     
-    // int Cost[10][10] = {  /* matrix : Example '02' Given by math sir...*/
-    //     {I , 8 , 2 , 5 , I , I , I , I }, 
-    //     {8 , I , I , 2 , I , 13, I , I }, 
-    //     {2 , I , I , 2 , 5 , I , I , I }, 
-    //     {5 , 2 , 2 , I , 1 , 6 , 3 , I }, 
-    //     {I , I , 5 , 1 , I , I , 1 , I }, 
-    //     {I , 13, I , 6 , I , I , 2 , 3 }, 
-    //     {I , I , I , 3 , 1 , 2 , I , 6 }, 
-    //     {I , I , I , I , I , 3 , 6 , I }
-    // };
+    int Cost[10][10] = {  /* matrix : Example '02' Given by math sir...*/
+        {I , 8 , 2 , 5 , I , I , I , I }, 
+        {8 , I , I , 2 , I , 13, I , I }, 
+        {2 , I , I , 2 , 5 , I , I , I }, 
+        {5 , 2 , 2 , I , 1 , 6 , 3 , I }, 
+        {I , I , 5 , 1 , I , I , 1 , I }, 
+        {I , 13, I , 6 , I , I , 2 , 3 }, 
+        {I , I , I , 3 , 1 , 2 , I , 6 }, 
+        {I , I , I , I , I , 3 , 6 , I }
+    };
 
-    shortestPath01(Cost, Distance, S, n); // Calls function to find the shortest paths...
+    // int Cost[10][10] = { /* Matrix : Example '01' Given by teacher... VERIFIED*/
+    //     {I , 50, 45, 10, I , I },
+    //     {I , I , 10, 15, I , I },
+    //     {I , I , I , I , 30, I },
+    //     {20, I , I , I , 15, I },
+    //     {I , 20, 35, I , I , I },
+    //     {I , I , I , I , 3 , I }
+    // };
+    shortestPath(Cost, Distance, S, n); // Calls function to find the shortest paths...
     cout << endl << endl;
 
     return(0);
@@ -75,51 +75,39 @@ void displayContent(int Cost[][10], int Distance[], bool S[], int n)
     }
     cout << endl << endl;
 }
-void shortestPath(int Cost[][10], int Distance[], bool S[], int n) 
+void shortestPath(int Cost[][10], int Distance[], bool S[], int n) // DOESNT WORK DO ENTIRE AGAIN...
 {
-    int v = 0, u, w; //Node -i.e- Vertice pointers...n
-    for(int i=0; i<n; i++) { // Initialization of 'S' and 'Distance'(-using Cost matrix) arrays...
+    int v=0, u;
+    for(int i=0; i<n; i++) {
+        Distance[i] = I;
         S[i] = false;
-        Distance[i] = Cost[v][i];
+    }
+    Distance[v] = 0;
+    
+    for(int i=0; i<n; i++) 
+    {
+        int temp_cost = I;
+        for(int j=0; j<n; j++) {
+            if((!S[j]) && (Distance[j]) < temp_cost) {
+                temp_cost = Distance[j];
+                u = j;
+            }
+        }
+        S[u] = true;
+        for(int j=0; j<n; j++) {
+            if(j == u) { continue; }
+            else if((Distance[u] + Cost[u][j]) < Distance[j]) { Distance[j] = Distance[u] + Cost[u][j]; }
+        }
+        for(int i=0; i<n; i++) { // To display the Distanceance array...
+                if(i == 0) {
+                    cout << endl << "Distances updated from " << u << " :" << endl;
+                }
+                cout << i << " : " << Distance[i] << endl;
+            }
     }
     displayContent(Cost, Distance, S, n);
-    S[v] = true; // Sets first node as visited -i.e- TRUE...
-    Distance[v] = 0;
-
-    // MAIN PROCEDURE (FINDING SHORTEST PATH)....
-    for(int i=1; i<n; i++) {
-        u = w;
-        static int counter = 0;
-        // if(counter == 0) {
-            int temp_cost_v = I; // Setting for temporary use...
-            for(int j=0; j<n; j++) { // Sets 'u' wrt 'v'...
-
-                if((S[j] != true) && (Cost[v][j] < temp_cost_v)) 
-                { 
-                    temp_cost_v = Cost[v][j];
-                    u = j; 
-                }
-            }
-            counter++;
-        // }
-        S[u] = true;
-        
-        int temp_cost_w = I; // Setting for temporary use...
-        for(int k=0; k<n; k++) { // Sets 'w' wrt 'u'...
-            if((S[k] != true) && (Cost[u][k] < temp_cost_w)) {
-                temp_cost_w = Cost[u][k];
-                w = k;
-            }
-        }
-        if(Distance[w] > Distance[u] + Cost[u][w]) {
-            Distance[w] = Distance[u] + Cost[u][w];
-            cout << "Edited shortest Distanceance : " << Distance[w] << "  ";
-        
-        }
-        cout << " { v = " << v << "; u = " << u << "; w = " << w << " }" << endl; // Displaying the nodes selected as 'u' and 'w'...
-    }
-    displayContent(Cost, Distance, S, n);  
 }
+
 void shortestPath01(int Cost[][10], int Distance[], bool S[], int n) {
 
     for(int i=0; i<n; i++) { // Setting all Distanceances to infinity and status to unvisited...
@@ -133,6 +121,7 @@ void shortestPath01(int Cost[][10], int Distance[], bool S[], int n) {
         for(int i=0; i<n; i++) {
             if(!S[i] && (minVertex == -1 || Distance[i] < Distance[minVertex])) {
                 minVertex = i; 
+                cout << "HERE : " << minVertex << endl;
             }
         }
         S[minVertex] = true;
