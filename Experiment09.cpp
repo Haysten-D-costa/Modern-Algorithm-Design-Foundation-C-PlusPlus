d19efab699ac26f5f6a88253c767e7643421753b
Haysten Dcosta <haystendc@gmail.com>
2023-05-12 18:33:40 +0530

    Added Comments for better comprehension...

--- Experiment09.cpp
+++ Experiment09.cpp
@@ -18,43 +18,52 @@
 #define MAX 20
 using namespace std;
 
-int m = 2; // Max colours assigned...
-int n = 4; // Number of vertices in graph...
-int x[10]; // Resultant vector (gives the colours used for each vertex)...
-int Graph[10][10] = { // Matrix form of the graph....
-    {0, 1, 0, 1},
-    {1, 0, 1, 0},
-    {0, 1, 0, 1},
-    {1, 0, 1, 0},
-};
+int m = 3;   // no. of colors available for graph coloring...
+int n = 4;   // Number of vertices in graph...
+int x[MAX];  // Resultant vector (Gives the colours used for each vertex -i.e- the possible solutions)...
 
-void mColouring(int k);
-void nextVertex(int k);
+int Graph[MAX][MAX];    // Adjacency matrix (of the graph)....
+void mColouring(int k); // Function to color the vertices, with possible colors...
+void nextVertex(int k); // Function to select nodes to be colored...
 
 int main() {
+    int k = 0; // Vertex (starting from 0th vertex)...
 
-    int k = 0; // Vertex visit(count)...
-    //todo cout << endl << "Enter no. of verteces : "; cin >> no_vertices;
-    
-    mColouring(k);
+    cout << endl << "Enter no. of verteces : "; cin >> n;
+    cout << "Enter no. of colours possible : "; cin >> m;
+    cout << endl << "Enter the adjacency matrix : ";
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin >> Graph[i][j];
+        }
+    }
+    mColouring(k); // k=0 -i.e- start colouring the graph from the 0th vertex...
     return(0);
 }
 void mColouring(int k) {
 
     while(1) 
     {
+        int static counter = 0;
         nextVertex(k);
-        if(x[k] == 0) { return; }
+        if(x[k] == 0) { 
+            return; 
+        }
         if(k == n) {
-
-            cout << endl << "Result Vector : (  ";
+            if(counter == 0) { 
+                cout << endl << "Possible solutions : "; 
+                counter ++; 
+            }
+            cout << endl << " ->  (  ";
             for(int i=0; i<n; i++) {
                 cout << x[i] << "  ";
             }
             cout << ")";
             return;
         }
-        else { mColouring(k+1); }
+        else { 
+            mColouring(k+1); 
+        }
     }
 }
 void nextVertex(int k) {
@@ -63,11 +72,16 @@ void nextVertex(int k) {
     while(1)
     {
         x[k] = (x[k] + 1) % (m + 1);
-        if(x[k] == 0) { return; }
-
-        for(j=0; j<n; j++) {
-            if((Graph[k][j] == 1) && (x[k] == x[j])) { break; }
+        if(x[k] == 0) { 
+            return; 
         }
-        if(j == n) { return; }
+        for(j=0; j<n; j++) {
+            if((Graph[k][j] == 1) && (x[k] == x[j])) { 
+                break; 
+            }
+        }
+        if(j == n) { 
+            return; 
+        }
     }
 }
