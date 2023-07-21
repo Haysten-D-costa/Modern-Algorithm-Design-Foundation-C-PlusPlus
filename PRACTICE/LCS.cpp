356818bcccbad60ed9194a1c4aa6d7e9f65c1fa4
Haysten Dcosta <haystendc@gmail.com>
2023-07-21 05:45:23 +0530

    Updated LCS Program

--- PRACTICE/LCS.cpp
+++ PRACTICE/LCS.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
 int LCS[10][10] {0};
+void findString(int LCS[][10], std::string S1, std::string S2, int len1, int len2) {
+
+    int index = LCS[len1][len2]; 
+	char lcs[index + 1];
+	lcs[index] = '\0'; 
+
+	int i = len1;
+    int j = len2;
+	
+    while (i > 0 && j > 0) {
+		
+        if (S1[i - 1] == S2[j - 1]) {
+			lcs[index - 1] = S1[i - 1];
+			i--;
+			j--;
+			index--; 
+		}
+		else if (LCS[i - 1][j] > LCS[i][j - 1])
+			i--;
+		else
+			j--;
+	}
+    std::cout << lcs << std::endl;
+}   
 void longestCS(std::string S1, std::string S2) {
     int len1 = S1.length();
     int len2 = S2.length();
@@ -16,6 +40,7 @@ void longestCS(std::string S1, std::string S2) {
         }
         std::cout << std::endl;
     }
+    findString(LCS, S1, S2, len1, len2);
 }
 int main() {
     std::string S1, S2;
