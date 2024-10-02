46ba38fd70fc2a91d64cc4777cabf3d05872acfa
Haysten-D-costa <haystendc@gmail.com>
2024-10-02 13:38:03 +0530

    final commit

---
+++ PracticalPractice/DBSystem.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+    <form action="">
+        <button>QUERY 1</button>
+    </form>
+    <form action="">
+        <button>QUERY 2</button>
+    </form>
+    <form action="">
+        <button>QUERY 3</button>
+    </form>
+    <form action="">
+        <button>QUERY 4</button>
+    </form>
+</body>
+</html>
